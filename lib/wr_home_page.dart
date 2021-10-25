ffa6ac4708094b386668101b6d9eb60826a30e9a
soojin <soojin@wgslabs.com>
2021-10-25 14:03:10 +0900

    Modified HomePage

--- lib/wr_home_page.dart
+++ lib/wr_home_page.dart
@@ -8,61 +8,77 @@ class wrHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Row(
-            children: [
-              Container(
-                child: Text('1.회사로고'),
-              ),
-              Container(child: Clock() //2
+      body: Padding(
+        padding: const EdgeInsets.all(30),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  child: Text('1.회사로고'),
+                ),
+                Container(child: Clock() //2
+                    ),
+                Container(
+                  child: Text('3.프로그램타이틀 레시피네임'),
+                ),
+                Container(
+                  child: Text('4.런/에러상태표시'),
+                ),
+                Container(
+                  child: Text('5.세팅버튼'),
+                ),
+                Container(
+                  child: Text('13.최소화버튼'),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Expanded(
+                  child: Container(
+                    width: 1000,
+                    height: 550,
+                    child: Text('6.차트뷰'),
                   ),
-              Container(
-                child: Text('3.프로그램타이틀 레시피네임'),
-              ),
-              Container(
-                child: Text('4.런/에러상태표시'),
-              ),
-              Container(
-                child: Text('5.세팅버튼'),
-              ),
-              Container(
-                child: Text('13.최소화버튼'),
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              Container(
-                child: Text('6.차트뷰'),
-              ),
-              Column(
-                children: [
-                  Container(
-                    child: Text('7.'),
-                  ),
-                  Row(
-                    children: [
-                      StartStopToggleBtn(),
-                      Container(
-                        child: Text('9.'),
-                      ),
-                    ],
-                  ),
-                  Container(
-                    child: Text('10.'),
-                  ),
-                  Container(
-                    child: Text('11.'),
-                  ),
-                  Container(
-                    child: Text('12.'),
-                  ),
-                ],
-              )
-            ],
-          ),
-        ],
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      height: 250,
+                      width: 150,
+                      color: Colors.amber,
+                      child: Text('7.'),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(child: Text('8')),
+                        StartStopToggleBtn(),
+                        Container(
+                          child: Text('9.'),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      child: Text('10.'),
+                    ),
+                    Container(
+                      child: Text('11.'),
+                    ),
+                    Container(
+                      child: Text('12.'),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
