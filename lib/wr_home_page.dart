366440e5fe4c82b0b6cec871e6fd95606901421b
sdkim <swksd01@gmail.com>
2021-11-14 13:47:17 +0900

    [FreqAI-001][2021.11.14][김신대] - 박스필

--- lib/wr_home_page.dart
+++ lib/wr_home_page.dart
@@ -27,7 +27,7 @@ class wrHomePage extends StatelessWidget {
                   height: Height * 0.1,
                   decoration: BoxDecoration(
                       border: Border.all(color: Colors.black, width: 1)),
-                  child: Image.asset("images/CI_nobg.png"),
+                  child: Image.asset("images/CI_nobg.png", fit: BoxFit.fill),
                 ),
                 Container(
                     padding: EdgeInsets.only(left: 27, top: 12),
